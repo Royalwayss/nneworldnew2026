62df8b0032eb6a67ffd5def9660d338243a58aad
Hxtreme <rwpttech@gmail.com>
2026-03-13 12:45:37 +0530

    config

---
+++ storage/framework/views/bb8df0f959aadd97ba2b7e9e0e1f3a89.php
@@ -0,0 +1,16 @@
+      <script src="<?php echo e(asset('front/assets/js/jquery-3.6.1.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/bootstrap.bundle.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/jquery.meanmenu.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/swiper-bundle.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/jquery.magnific-popup.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/counter.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/progressbar.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/aos.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/gsap.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/SplitText.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/ScrollSmoother.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/ScrollTrigger.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/vanilla-tilt.min.js')); ?>"></script>
+      <script src="<?php echo e(asset('front/assets/js/main.js')); ?>?v=2.7"></script>
+      <script src="<?php echo e(asset('front/assets/js/custom.js')); ?>"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script><?php /**PATH D:\xampp\htdocs\nneworldnew2026\nneworldnew2026\resources\views/front/elements/script.blade.php ENDPATH**/ ?>
