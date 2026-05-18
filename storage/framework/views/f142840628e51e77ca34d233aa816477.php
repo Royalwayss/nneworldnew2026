06db89f425b41a0fb413b7c1f0b17997b4e6ce0f
Hxtreme <rwpttech@gmail.com>
2026-05-18 10:50:00 +0530

    change

--- storage/framework/views/f142840628e51e77ca34d233aa816477.php
+++ storage/framework/views/f142840628e51e77ca34d233aa816477.php
@@ -12,4 +12,4 @@
       <link rel="stylesheet" href="<?php echo e(asset('front/assets/css/progressbar.css')); ?>">
       <link rel="stylesheet" href="<?php echo e(asset('front/assets/css/master.css')); ?>?v=3.2">
       <link rel="stylesheet" href="<?php echo e(asset('front/assets/css/responsive.css')); ?>?v=3.0">
-      <link rel="stylesheet" href="<?php echo e(asset('front/assets/css/custom.css')); ?>?v=3.1"><?php /**PATH D:\xampp\htdocs\nneworldnew2026\nneworldnew2026\resources\views/front/elements/style.blade.php ENDPATH**/ ?>
+      <link rel="stylesheet" href="<?php echo e(asset('front/assets/css/custom.css')); ?>?v=3.2"><?php /**PATH D:\xampp\htdocs\nneworldnew2026\nneworldnew2026\resources\views/front/elements/style.blade.php ENDPATH**/ ?>
