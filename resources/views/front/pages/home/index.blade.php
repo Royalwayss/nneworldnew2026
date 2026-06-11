2f9a79da16831f8e7b099a9c82ad6ee6299a687a
Hxtreme <rwpttech@gmail.com>
2026-06-11 14:53:07 +0530

    home-page-change

--- resources/views/front/pages/home/index.blade.php
+++ resources/views/front/pages/home/index.blade.php
@@ -580,7 +580,7 @@
                      <div class="swiper-slide">
                         <div class="brand__slide">
                            <a href="javascript:;" <?php /*target="_blank" href="https://www.volare-bicycles.com/" */ ?>>
-                              <img src="front/assets/imgs/brand/volare-bicycles.jpeg" alt="img" title="img">
+                              <img src="front/assets/imgs/brand/volare-bicycles.png" alt="img" title="img">
                            </a>
                         </div>
                      </div>
